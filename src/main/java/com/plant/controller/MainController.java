479562154d56e02e5a5ab562da7aac50e056a3e3
bellaluna5084 <silverwesten@gmail.com>
2023-05-03 10:05:56 +0900

    after abort

--- src/main/java/com/plant/controller/MainController.java
+++ src/main/java/com/plant/controller/MainController.java
@@ -1,18 +1,12 @@
 package com.plant.controller;
 
-import java.sql.SQLException;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.plant.service.PostService;
-import com.plant.vo.PostVo;
 
 @RestController
 public class MainController {
